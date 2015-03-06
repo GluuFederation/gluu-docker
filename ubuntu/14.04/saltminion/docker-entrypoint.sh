ecde37a772d59bba152b409f1775ec1d8dfb2fbe
axetwe <axetwe@gmail.com>
2015-03-06 19:08:30 +0300

    No quotes

--- ubuntu/14.04/saltminion/docker-entrypoint.sh
+++ ubuntu/14.04/saltminion/docker-entrypoint.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-# Keeping it low-key for now, shouldn't affect current setup.
-echo $SALT_MASTER_IP || echo "SALT_MASTER_IP isn't set"
-sudo sed -i /etc/salt/minion-stub -e "s/REPLACE_ME/$SALT_MASTER_IP/" || exit 0
+echo $SALT_MASTER_IP
+sudo sed -i /etc/salt/minion-stub -e s/REPLACE_ME/$SALT_MASTER_IP/ || exit 0
 
 /usr/bin/salt-minion
