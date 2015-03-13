28a8cd814f7883e39340ee57d5bf22c8ae1a7550
shouro <sourogts@gmail.com>
2015-03-13 08:11:37 +0600

    adding supervisor to manages processes

--- ubuntu/14.04/saltminion/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sed -i /etc/salt/minion-stub -e s/REPLACE_ME/$SALT_MASTER_IPADDR/ || exit 0
-cat /etc/salt/minion-stub || exit 0
-echo $SALT_MASTER_IPADDR && cp /etc/salt/minion-stub /etc/salt/minion
-
-/usr/bin/salt-minion
