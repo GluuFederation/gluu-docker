82a3b41716ea22b608ddb57616f7bb7b9be0da50
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-08-07 12:20:40 +0700

    bind to port 8080 by default

--- ubuntu/14.04/gluuengine/gunicorn.conf.py
+++ ubuntu/14.04/gluuengine/gunicorn.conf.py
@@ -1,10 +1,10 @@
 import multiprocessing
 
-bind = ":8000"
+bind = ":8080"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = workers
 worker_class = 'gthread'
 loglevel = 'warning'
 accesslog = '-'
 errorlog = '-'
-raw_env = 'API_ENV=prod' # 'prod|test|dev'
+raw_env = 'API_ENV=prod'  # 'prod|test|dev'
