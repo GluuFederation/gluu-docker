a7877990a14773a1d24a7f54e9a7e4843250ac5d
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-12-09 14:29:54 +0700

    message-consumer: modify entrypoint to use external .properties file (if any)

--- ubuntu/14.04/message-consumer/docker-entrypoint.sh
+++ ubuntu/14.04/message-consumer/docker-entrypoint.sh
@@ -2,48 +2,17 @@
 set -e
 
 if [ "$1" = 'message-consumer' ]; then
-    # if there's a custom config file, load it
-    if [ -f /etc/message-consumer/config.sh ]; then
-        source /etc/message-consumer/config.sh
+    SPRING_EXTERNAL_PROPERTIES=/etc/message-consumer/application-gluu.properties
+
+    if [ -f $SPRING_EXTERNAL_PROPERTIES ]; then
+        # if there's an external properties file, load it
+        APP_OPTS="--spring.config.location=/etc/message-consumer/ --spring.config.name=application-gluu"
+    else
+        # fallback to default properties
+        APP_OPTS="--spring.profiles.active=prod-mysql"
     fi
 
-    if [ ! -z $SPRING_PROFILES_ACTIVE ]; then
-        SPRING_PROFILES_ACTIVE_OPT="--spring.profiles.active=${SPRING_PROFILES_ACTIVE}"
-    fi
-
-    if [ ! -z $SPRING_ACTIVEMQ_BROKER_URL ]; then
-        SPRING_ACTIVEMQ_BROKER_URL_OPT="--spring.activemq.broker-url=${SPRING_ACTIVEMQ_BROKER_URL}"
-    fi
-
-    if [ ! -z $SPRING_ACTIVEMQ_USER ]; then
-        SPRING_ACTIVEMQ_USER_OPT="--spring.activemq.user=${SPRING_ACTIVEMQ_USER}"
-    fi
-
-    if [ ! -z $SPRING_ACTIVEMQ_PASSWORD ]; then
-        SPRING_ACTIVEMQ_PASSWORD_OPT="--spring.activemq.password=${SPRING_ACTIVEMQ_PASSWORD}"
-    fi
-
-    if [ ! -z $SPRING_DATASOURCE_URL ]; then
-        SPRING_DATASOURCE_URL_OPT="--spring.datasource.url=${SPRING_DATASOURCE_URL}"
-    fi
-
-    if [ ! -z $SPRING_DATASOURCE_USERNAME ]; then
-        SPRING_DATASOURCE_USERNAME_OPT="--spring.datasource.username=${SPRING_DATASOURCE_USERNAME}"
-    fi
-
-    if [ ! -z $SPRING_DATASOURCE_PASSWORD ]; then
-        SPRING_DATASOURCE_PASSWORD_OPT="--spring.datasource.password=${SPRING_DATASOURCE_PASSWORD}"
-    fi
-
-    # run the app
-    exec java -jar /opt/message-consumer-0.0.1-SNAPSHOT.jar \
-        $SPRING_PROFILES_ACTIVE_OPT \
-        $SPRING_ACTIVEMQ_BROKER_URL_OPT \
-        $SPRING_ACTIVEMQ_USER_OPT \
-        $SPRING_ACTIVEMQ_PASSWORD_OPT \
-        $SPRING_DATASOURCE_URL_OPT \
-        $SPRING_DATASOURCE_USERNAME_OPT \
-        $SPRING_DATASOURCE_PASSWORD_OPT
+    exec java -jar /opt/message-consumer-0.0.1-SNAPSHOT.jar $APP_OPTS
 fi
 
 # run everything else
