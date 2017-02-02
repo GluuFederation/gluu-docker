c503d033da95f3248c75acb0d8cf05fd28d98203
shouro <sourogts@gmail.com>
2017-02-03 02:27:53 +0600

    many small fixes

--- ubuntu/14.04/oxeleven/_oxeleven/setup_n_render.sh
+++ ubuntu/14.04/oxeleven/_oxeleven/setup_n_render.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-softhsm2-util --init-token --slot 0 --label "detoken" --so-pin 1234 --pin 1234
+PIN=1234
+softhsm2-util --init-token --slot 0 --label "detoken" --so-pin $PIN --pin $PIN
 slotid=$(softhsm2-util --show-slots | head -n 2 | tail -n 1 | sed 's/Slot //g')
-cat /tmp/oxeleven-config.json.tmpl | sed -e "s/\oxPIN/$pin/" -e "s/\oxSLOT/$slotid/" - > /tmp/oxeleven-config.json
+cat /tmp/oxeleven-config.json.tmpl | sed -e "s/\oxPIN/$PIN/" -e "s/\oxSLOT/$slotid/" - > /tmp/oxeleven-config.json
