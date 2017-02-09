d6f70bb5af7f12926d31ebc8be67ad99e660e7e8
shouro <sourogts@gmail.com>
2017-02-09 06:51:27 +0600

    runtime randoom pin genaration for softHSM2

---
+++ ubuntu/14.04/oxeleven/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ ! -f /touched ]; then
+    touch /touched
+    pin=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 6)
+    softhsm2-util --init-token --slot 0 --label "detoken" --so-pin $pin --pin $pin
+    slotid=$(softhsm2-util --show-slots | head -n 2 | tail -n 1 | sed 's/Slot //g')
+    cat /tmp/oxeleven-config.json.tmpl | sed -e "s/\oxPIN/$pin/" -e "s/\oxSLOT/$slotid/" - > /etc/gluu/conf/oxeleven-config.json
+fi
+
+exec gosu root /usr/bin/supervisord
--- ubuntu/14.04/oxeleven/setup_n_render.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PIN=1234
-softhsm2-util --init-token --slot 0 --label "detoken" --so-pin $PIN --pin $PIN
-slotid=$(softhsm2-util --show-slots | head -n 2 | tail -n 1 | sed 's/Slot //g')
-cat /tmp/oxeleven-config.json.tmpl | sed -e "s/\oxPIN/$PIN/" -e "s/\oxSLOT/$slotid/" - > /tmp/oxeleven-config.json
