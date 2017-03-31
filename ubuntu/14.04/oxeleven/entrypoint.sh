8efd587bfa7677cc28533d37500d26ea7c2037d2
shouro <sourogts@gmail.com>
2017-04-01 00:20:05 +0600

    updating ox11 dockerfile to support token

--- ubuntu/14.04/oxeleven/entrypoint.sh
+++ ubuntu/14.04/oxeleven/entrypoint.sh
@@ -6,7 +6,8 @@ if [ ! -f /touched ]; then
     pin=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | head --bytes 6)
     softhsm2-util --init-token --slot 0 --label "detoken" --so-pin $pin --pin $pin
     slotid=$(softhsm2-util --show-slots | head -n 2 | tail -n 1 | sed 's/Slot //g')
-    cat /tmp/oxeleven-config.json.tmpl | sed -e "s/\oxPIN/$pin/" -e "s/\oxSLOT/$slotid/" - > /etc/gluu/conf/oxeleven-config.json
+    [[ -z "${OXUUID}" ]] && uuid=$(cat /proc/sys/kernel/random/uuid) || uuid="${OXUUID}"
+    cat /tmp/oxeleven-config.json.tmpl | sed -e "s/\oxPIN/$pin/" -e "s/\oxSLOT/$slotid/" -e "s/\oxUUID/$uuid/" - > /etc/gluu/conf/oxeleven-config.json
 fi
 
 exec gosu root /usr/bin/supervisord
