987377c8304f0989a8730ece2728dded5653b378
iromli <isman.firmansyah@gmail.com>
2018-09-27 06:37:24 +0700

    multihosts: use 3.1.4_dev

--- examples/multi-hosts/config.sh
+++ examples/multi-hosts/config.sh
@@ -35,7 +35,7 @@ bootstrap_config() {
                 -v /opt/config-init/db:/opt/config-init/db/ \
                 -e GLUU_CONFIG_ADAPTER=consul \
                 -e GLUU_CONSUL_HOST=consul.server \
-                gluufederation/config-init:latest \
+                gluufederation/config-init:3.1.4_dev \
                 load
         else
             generate_config
@@ -75,7 +75,7 @@ generate_config() {
         -v /opt/config-init/db:/opt/config-init/db/ \
         -e GLUU_CONFIG_ADAPTER=consul \
         -e GLUU_CONSUL_HOST=consul.server \
-        gluufederation/config-init:latest \
+        gluufederation/config-init:3.1.4_dev \
         generate \
         --admin-pw secret \
         --email $email \
