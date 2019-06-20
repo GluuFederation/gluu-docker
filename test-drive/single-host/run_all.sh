ffa4ea2d13fad5ddd1cb0baf743ca7d89d907c07
Mohammad Abudayyeh <47318409+mogluu@users.noreply.github.com>
2019-06-20 17:46:12 +0000

    Update run_all.sh

--- test-drive/single-host/run_all.sh
+++ test-drive/single-host/run_all.sh
@@ -326,8 +326,8 @@ get_files
 mkdir -p $CONFIG_DIR
 touch vault_role_id.txt
 touch vault_secret_id.txt
-# touch gcp_kms_stanza.hcl
-# touch gcp_kms_creds.json
+touch gcp_kms_stanza.hcl
+touch gcp_kms_creds.json
 
 gather_ip
 until confirm_ip; do : ; done
