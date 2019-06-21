75c8b72c3eaf811a88ee77e9806dc83f917ef852
Mohammad Abudayyeh <47318409+mogluu@users.noreply.github.com>
2019-06-21 06:13:43 +0000

    added overwrite to wget

--- test-drive/single-host/run_all.sh
+++ test-drive/single-host/run_all.sh
@@ -16,8 +16,8 @@ DOCKER_COMPOSE=${DOCKER_COMPOSE:-docker-compose}
 DOCKER=${DOCKER:-docker}
 
 get_files(){
-    wget https://raw.githubusercontent.com/GluuFederation/gluu-docker/3.1.6/test-drive/single-host/docker-compose.yml
-    wget https://raw.githubusercontent.com/GluuFederation/gluu-docker/3.1.6/test-drive/single-host/vault_gluu_policy.hcl
+    wget -q https://raw.githubusercontent.com/GluuFederation/gluu-docker/3.1.6/test-drive/single-host/docker-compose.yml -O 
+    wget -q https://raw.githubusercontent.com/GluuFederation/gluu-docker/3.1.6/test-drive/single-host/vault_gluu_policy.hcl -O 
 }
 
 gather_ip() {
